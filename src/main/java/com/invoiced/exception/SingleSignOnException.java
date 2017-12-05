f78ef5c307606f3df9489832447fae6cee4a9d78
Jared King <j@jaredtking.com>
2017-12-05 14:08:07 -0600

    implement single sign-on token generation - resolves #12

---
+++ src/main/java/com/invoiced/exception/SingleSignOnException.java
@@ -0,0 +1,9 @@
+package com.invoiced.exception;
+
+public class SingleSignOnException extends InvoicedException {
+	private static final long serialVersionUID = 1L;
+
+	public SingleSignOnException(Throwable cause) {
+		super(cause);
+	}
+}
